6d7225c7fb17f9be4039fe584ef89e2e7ac0c29b
liondot <51257576+liondot@users.noreply.github.com>
2022-04-25 18:24:50 +0900

    할일 list 고유 id 랜덤 지정

--- dotolist_1/main.js
+++ dotolist_1/main.js
@@ -3,6 +3,9 @@
 // + 버튼을 클릭하면, 할일이 추가된다. 
 // 유저가 del버튼 누르면 삭제된다. 
 // check버튼 누르면 할일완료, line-through 생성
+//  1. check버튼을 클릭하는 순간 true를 false로바꿔주기
+//  2. true이면 끝난걸로 간주하고 밑줄 보여주기 
+//  3. false이면 안끝난걸로 간주하고 그대로 
 // 진행중 끝남 탭을 누르면, 언더바가 이동
 // 완료탭과 진행중 탭에 분류되어진다. 
 // 전체 탭을 누르면 전체 아이템으로 돌아옴 
@@ -15,25 +18,41 @@ let taskList = []
 addBtn.addEventListener("click", addTask);
 
 function addTask() {
-  let taskContent = taskInput.value;
-  taskList.push(taskContent);
+  let task = {
+    id: randomIDGenerate(),
+    taskContent: taskInput.value,
+    isComplete: false
+  }
+  taskList.push(task);
   console.log(taskList);
   rander()
 }
 
 function rander() {
   let resultHTML = '';
-  for(let i = 0; i < taskList.length; i++) {
+  for (let i = 0; i < taskList.length; i++) {
     resultHTML += `<div class="task">
-    <div>${taskList[i]}</div>
+    <div>${taskList[i].taskContent}</div>
     <div>
-      <button>check</button>
+      <button onclick="toggleComplete('${taskList[i].id}')">check</button>
       <button>Delete</button>
     </div>
-  </div>`
+  </div>`;
   }
   document.getElementById("task_board").innerHTML = resultHTML;
 }
 
 
+function toggleComplete(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id == id) {
+      taskList[i].isComplete = true
+      break;
+    }
+  }
+  console.log(taskList)
+}
 
+function randomIDGenerate() {
+  return '_' + Math.random().toString(36).substr(2, 9);
+}
