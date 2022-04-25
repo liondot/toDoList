9ac1ffdc13a260291148e1c8a523ba3faee94c93
liondot <51257576+liondot@users.noreply.github.com>
2022-04-25 16:50:17 +0900

    스펠링 에러 확인하기

---
+++ dotolist_1/main.js
@@ -0,0 +1,17 @@
+// ++++to do list 로직 정리 ++++
+// 유저가 값을 입력한다. 
+// + 버튼을 클릭하면, 할일이 추가된다. 
+// 유저가 del버튼 누르면 삭제된다. 
+// check버튼 누르면 할일완료, line-through 생성
+// 진행중 끝남 탭을 누르면, 언더바가 이동
+// 완료탭과 진행중 탭에 분류되어진다. 
+// 전체 탭을 누르면 전체 아이템으로 돌아옴 
+
+let taskInput = document.getElementById('task_input');
+let addBtn = document.getElementById('add_button');
+
+addBtn.addEventListener("click", add);
+
+function add() {
+  console.log("add")
+}
