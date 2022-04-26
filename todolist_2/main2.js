62b2623e41d27014624a32ee5aa10575e6918b1d
liondot <51257576+liondot@users.noreply.github.com>
2022-04-26 19:01:45 +0900

    저장용 로직작성

--- todolist_2/main2.js
+++ todolist_2/main2.js
@@ -1,6 +1,10 @@
 // 1. 유저가 값을 입력한다. 
 // 2. +버튼을 클릭하면 할일이 추가된다. 
-
+// delete 버튼을ㅇ 누르면 할일이 삭제된다 
+// check버튼을 누르면 할일이 끝나면서 밑줄이 간다. 
+// 진행중 끝남 탭을 누르면, 언더바가 이동한다. 
+// 끝남탭은, 끝난 아이템만, 진행중탭은 진행중인 아이템만 
+// 전체탭을 누르면 다시 전체아이템으로 돌아옴 
 
 let inputArea = document.getElementById("input_area");
 console.log(inputArea)
@@ -10,5 +14,4 @@ addBtn.addEventListener("click", add)
 
 function add() {
   let inputAreaValue = inputArea.value;
-  
 }
