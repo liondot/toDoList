d4a4a723671c413dc879c36b26cac3b1559ef37c
liondot <51257576+liondot@users.noreply.github.com>
2022-04-26 18:35:57 +0900

    second time file setting

---
+++ todolist_2/main2.js
@@ -0,0 +1,14 @@
+// 1. 유저가 값을 입력한다. 
+// 2. +버튼을 클릭하면 할일이 추가된다. 
+
+
+let inputArea = document.getElementById("input_area");
+console.log(inputArea)
+let addBtn = document.querySelector("#add_button");
+
+addBtn.addEventListener("click", add) 
+
+function add() {
+  let inputAreaValue = inputArea.value;
+  
+}
