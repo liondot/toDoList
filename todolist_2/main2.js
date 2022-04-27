e31ef05b5880306c05630db6b37ef9f915cdd3bf
liondot <51257576+liondot@users.noreply.github.com>
2022-04-27 18:04:57 +0900

    second time : addBtn, taskList 생성, UI구현

--- todolist_2/main2.js
+++ todolist_2/main2.js
@@ -9,9 +9,33 @@
 let inputArea = document.getElementById("input_area");
 console.log(inputArea)
 let addBtn = document.querySelector("#add_button");
+let taskList = [];
+addBtn.addEventListener("click", addTask) 
 
-addBtn.addEventListener("click", add) 
 
-function add() {
-  let inputAreaValue = inputArea.value;
+function addTask() {
+  let inputValue = inputArea.value;
+  taskList.push(inputValue);
+  render()
+  console.log(taskList)
 }
+
+function render() {
+  let resultHTML = ''
+  for(let i = 0; i<taskList.length; i++) {
+    resultHTML += ` <div id="task_board">
+    <div class="task_list">
+      <div>${taskList[i]}</div>
+      <div>
+        <button>check</button>
+        <button>delete</button>
+      </div>
+    </div>
+  </div>
+    `
+  }
+
+  document.querySelector('#task_board').innerHTML = resultHTML;
+}
+
+
