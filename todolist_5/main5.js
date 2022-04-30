31e4fc971ffa7d62f31f9aae5e71d39e85968079
liondot <51257576+liondot@users.noreply.github.com>
2022-04-30 16:47:08 +0900

    에러 확인해야 함

--- todolist_5/main5.js
+++ todolist_5/main5.js
@@ -1,8 +1,18 @@
 let inputArea = document.querySelector('#input_area');
 let addBtn = document.querySelector('#addBtn');
 let taskList = [];
+let tabs = document.querySelectorAll(".tabs a");
+let mode = ""
+let filterList = []
+
 
 addBtn.addEventListener('click', addTask);
+
+for(let i = 0; i<tabs.length; i++){
+  tabs[i].addEventListener("click", function(e) {
+    filter(e)
+  })
+}
 
 function addTask() {
   task = {
@@ -17,23 +27,30 @@ function addTask() {
 }
 
 function render() {
+  let list = []
+  if(mode == "all"){
+    list = taskList
+  } else if (mode == "ongoing" || mode == "done") {
+    list = filterList
+  }
+
   let resultHTML = ''
-  for (let i = 0; i < taskList.length; i++) {
-    if(taskList[i].isComplete == true) {
+  for (let i = 0; i < list.length; i++) {
+    if(list[i].isComplete == true) {
       resultHTML += `<div class="task">
-      <div class="task_done">${taskList[i].taskContents}</div>
+      <div class="task_done">${list[i].taskContents}</div>
       <div>
-        <button onclick="toggleComplete('${taskList[i].id}')">check</button>
-        <button onclick="deleteTask('${taskList[i].id}')">delete</button>
+        <button onclick="toggleComplete('${list[i].id}')">check</button>
+        <button onclick="deleteTask('${list[i].id}')">delete</button>
       </div>
     </div>`
     } else {
       resultHTML += `
       <div class="task">
-            <div>${taskList[i].taskContents}</div>
+            <div>${list[i].taskContents}</div>
             <div>
-              <button onclick="toggleComplete('${taskList[i].id}')">check</button>
-              <button onclick="deleteTask('${taskList[i].id}')">delete</button>
+              <button onclick="toggleComplete('${list[i].id}')">check</button>
+              <button onclick="deleteTask('${list[i].id}')">delete</button>
             </div>
           </div>`
     }
@@ -45,7 +62,6 @@ function render() {
 }
 
 function toggleComplete(id){
-  console.log("id :", id)
   for(let i = 0; i<taskList.length; i++) {
     if(taskList[i].id == id){
       taskList[i].isComplete = !taskList[i].isComplete;
@@ -55,15 +71,36 @@ function toggleComplete(id){
 }
 
 function deleteTask(id){
-  console.log('id', id)
   for(let i = 0; i<taskList.length; i++){
     if(taskList[i].id == id) {
       taskList.splice(i, 1)
     }
   }
-  render()
+  filter()
 }
 
+function filter(e){
+  console.log("filter 클릭", e.target.id);
+  mode = e.target.id
+  if(mode == all) {
+    render()
+  } else if (mode == ongoing ) {
+    for(let i = 0; i<taskList.length; i++){
+      if(taskList[i].isComplete == false) {
+        filterList.push(taskList[i])
+      }
+    }
+  render()
+  } else if (mode == done) {
+    for(let i = 0; i<taskList.length; i++){
+      if(taskList[i].isComplete == true) {
+        filterList.push(taskList[i])
+    }
+  }
+}
+filter();
+
+}
 function randomIDGenerate() {
   return "_" + Math.random().toString(36). substr(2,9);
 }
