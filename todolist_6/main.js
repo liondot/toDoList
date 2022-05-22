27867ee5fb75cfb676785b5be5c2a3c41c071903
liondot <51257576+liondot@users.noreply.github.com>
2022-05-22 16:15:03 +0900

    텝영역 ongoing에러 확인

--- todolist_6/main.js
+++ todolist_6/main.js
@@ -9,10 +9,13 @@
 // 끝난탭은, 끝난 아이템만 진행중텝은 진행중인 아이템만 나온다. 
 // 전체탭을 누르면 다시 전체아이템으로 돌아옴 
 
-
 let taskInput = document.getElementById("task_input");
 let addBtn = document.getElementById("add_btn")
 let taskList = []
+let tabs = document.querySelectorAll(".task_tabs div");
+let mode = ''
+let filterList = []
+
 addBtn.addEventListener("click", addTask);
 
 function addTask() {
@@ -26,31 +29,60 @@ function addTask() {
 }
 
 function render() {
+  let list = []
+  if(mode == "all") {
+    list = taskList
+  } else if ( mode == "ongoing"){
+    list = filterList
+  }
   let resultHTML = ''
-  for(let i = 0; i<taskList.length; i++){
-  if(taskList[i].isComplete == true) {
-    resultHTML += `
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].isComplete == true) {
+      resultHTML += `
     <div class="task">
-      <div class="task_done">${taskList[i].taskContent}</div>
+      <div class="task_done">${list[i].taskContent}</div>
       <div>
-        <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
-        <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
+        <button onclick="toggleComplete('${list[i].id}')">Check</button>
+        <button onclick="deleteTask('${list[i].id}')">Delete</button>
       </div>
     </div>
     `
-  } else {
-    resultHTML += `
+    } else {
+      resultHTML += `
       <div class="task">
-         <div>${taskList[i].taskContent}</div>
+         <div>${list[i].taskContent}</div>
          <div>
-           <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
-           <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
+           <button onclick="toggleComplete('${list[i].id}')">Check</button>
+           <button onclick="deleteTask('${list[i].id}')">Delete</button>
          </div>
        </div>`
+    }
   }
-}
   document.getElementById("task_board").innerHTML = resultHTML
 }
+
+
+for (let i = 1; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", function (event) {
+    filter(event)
+  });
+}
+
+function filter(event) {
+  mode = event.target.id;
+  filterList = [];
+  if (mode == "all") {
+    render()
+  } else if (mode == "ongoing") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete == false) {
+        filterList.push(taskList[i])
+      }
+    }
+    render()
+  }
+}
+
 
 function toggleComplete(id) {
   for (let i = 0; i < taskList.length; i++) {
